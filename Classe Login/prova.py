d9fdc7647c1c02cee33f11671ac4ce866f67f40a
f-lost <83578401+f-lost@users.noreply.github.com>
2024-11-11 09:59:17 +0100

    update

--- Classe Login/prova.py
+++ Classe Login/prova.py
@@ -6,53 +6,50 @@ from modulo_login import Utente, SistemaLogin
 
 
 def registrazione():
-
-    username = input("Inserisci l'username").lower()
-    if not login.controlla_utente(username):
+    username = input("Inserisci l'username: ").lower()
+    if not sistema_login.controlla_utente(username):
         password = input("Inserisci una password: ").lower()
         domanda_segreta = input("Qual era il nome della tua scuola elementare?: ").lower()
-        login.crea_nuovo_utente(username, password, domanda_segreta)
+        sistema_login.crea_nuovo_utente(username, password, domanda_segreta)
     else:
-        print("Utente già registrato, inserisci la password: ")
-        password = input("Inserisci la password: ").lower()
-        login.login(username, password)
+        print("Utente già registrato.")
 
-def login():
-    
-    username = input("Inserisci l'username") 
-    if login.controlla_utente(username):
 
+def effettua_login():
+    username = input("Inserisci l'username: ")
+    if sistema_login.controlla_utente(username):
         password = input("Inserisci la password: ")
-        login.login(username, password)
-    
-    else: 
+        check = sistema_login.effettua_login(username, password)
+        if check:
+            menulogin.mostra_menu()
 
-        print("Utente non trovato. Registrarsi.")
-        menu.mostra_menu()
+    else:
+        print("Utente non trovato.")
+
 
 def reset_password():
-
-    login.reset_password(username)
-
-def cambia_username():
-
-    login.cambia_username(old_username, new_username)
-
+    username = input("Inserisci l'username: ")
+    if sistema_login.controlla_utente(username):
+        sistema_login.reset_password(username)
+    else:
+        print("Utente non trovato.")
 
 
 
-menu = Menu()
-login = SistemaLogin()
+# Initialzza sistemi e menu
+sistema_login = SistemaLogin()
+menu_iniziale = Menu()
+menulogin = Menu()
 
+# Costruzione del menù iniziale
+menu_iniziale.aggiungi_elemento(Elemento("Registrazione", Azione(registrazione)))
+menu_iniziale.aggiungi_elemento(Elemento("Login", Azione(effettua_login)))
+menu_iniziale.aggiungi_elemento(Elemento("Reimposta Password", Azione(reset_password)))
 
+# Costruzione del menù dopo il login
+elemento1_menulogin = Elemento("MENU LOGIN")
+elemento2_menulogin = Elemento("Prova")
+elemento3_menulogin = Elemento("Prova")
 
-elemento1 = Elemento("Registrazione", Azione(registrazione))
-elemento2 = Elemento("Login", Azione(login))
-elemento3 = Elemento("Ringraziamento", Azione(ringrazia))
-
-menu.aggiungi_elemento(elemento1)
-menu.aggiungi_elemento(elemento2)
-menu.aggiungi_elemento(elemento3)
-
-# Mostra il menu ripetibile
-menu.mostra_menu()
+# Mostra il menù iniziale
+menu_iniziale.mostra_menu()
